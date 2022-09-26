b86e268d82a6b612c7027ec5c2eee0248e45e859
PaulSirk <sirkmorato@gmail.com>
2022-09-26 13:40:07 -0500

    binaryList.py now has look function added

--- binaryList.py
+++ binaryList.py
@@ -1,16 +1,21 @@
 class binaryList:
     def __init__(self, dataType):
         self.binaryList = []
+        self.length = 0
         self.dataType = dataType
 
     def __check_class(self, value) -> None:
         if type(value) != self.dataType:
             raise ValueError("Incorrect Data Type")
 
+    '''def __check_int(self, num) -> None:
+        if type(num) != int or num != None:
+            raise ValueError("Input a number.")'''
+
     # returns -1 if element is not found
     def search(self, search_num) -> int:
         self.__check_class(search_num)
-        binaryLength : int = len(self.binaryList)
+        binaryLength : int = self.length
 
         not_found : int = -1
 
@@ -36,9 +41,9 @@ class binaryList:
 
         return not_found
 
-    def insert_element(self, element) -> None:
+    def insert(self, element) -> None:
         valid = self.__check_class(element)
-        binaryLength : int = len(self.binaryList)
+        binaryLength : int = self.length
 
         if binaryLength == 0:
             self.binaryList.insert(0, element)
@@ -56,24 +61,42 @@ class binaryList:
             else:
                 center = binaryLength // 2
                 if self.binaryList[center] <= element:
-                    for i in range(center, binaryLength):
+                    for i in range(0, center, -1):
                         if self.binaryList[i] <= element:
                             self.binaryList.insert(i+1, element)
-                            return
+                            break
                 else:
-                    for j in range(0, center):
+                    for j in range(center, binaryLength):
                         if self.binaryList[j] >= element:
                             self.binaryList.insert(j, element)
-                            return
+                            break
+        self.length += 1
+        return
 
-            return
-
-    def delete_element(self, delete_num) -> None:
+    def delete(self, delete_num) -> None:
         present_num : int = self.search(delete_num)
         if present_num == -1:
             print("Element is not present.")
         else:
             del self.binaryList[present_num]
-                
+            self.length -= 1
         return
 
+    def look(self, start, end = None, steps = None) -> None:
+        if start == None:
+            raise ValueError("start can not be 'None'")
+
+        if end == None or start == end:
+            print(self.binaryList[start])
+        elif end != None and steps == None:
+            print(self.binaryList[start:end])
+        else:
+            print(self.binaryList[start:end:steps])
+        return
+
+x = binaryList(int)
+
+x.insert(0)
+x.insert(3)
+x.insert(2)
+x.insert(1)
